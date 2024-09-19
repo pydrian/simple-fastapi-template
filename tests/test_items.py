87190d37bb902cfeb8396c842459ca773c822564
Adrian Croitor <adriancroitor.93@gmail.com>
2024-09-19 18:09:58 +0300

    testing jenkins CI, fixing flake8 issues to see if it will build succesfully

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,8 +1,6 @@
 from fastapi.testclient import TestClient
 from api.main import app
 
-import httpx                                                                            # this is intentionally to import unused module
-
 
 client = TestClient(app)
 
@@ -13,5 +11,5 @@ def test_v1_items():
 
 
 def test_v2_items():
-    response = client.get("/v2/items/3")                                                # this is intentionally to break the flake8 line length
+    response = client.get("/v2/items/1")
     assert response.status_code in (200, 404)
